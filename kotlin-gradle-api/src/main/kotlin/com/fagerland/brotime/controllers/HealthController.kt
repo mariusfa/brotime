a08edc2180f7b82a00fca3507f76e795cd56b517
Marius Fagerland <mariusfagerland@yahoo.no>
2020-01-29 22:55:19 +0100

    Add crossorigin to healthcontroller

--- kotlin-gradle-api/src/main/kotlin/com/fagerland/brotime/controllers/HealthController.kt
+++ kotlin-gradle-api/src/main/kotlin/com/fagerland/brotime/controllers/HealthController.kt
@@ -1,8 +1,10 @@
 package com.fagerland.brotime.controllers
 
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+@CrossOrigin
 @RestController
 class HealthController {
 
