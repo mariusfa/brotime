caa53de0c4a04f476a1bd231804117ffcadfda5e
Marius Fagerland <mariusfagerland@yahoo.no>
2020-02-10 22:43:03 +0100

    Finish login page

--- react-web-typescript/src/app/LoginPage.tsx
+++ react-web-typescript/src/app/LoginPage.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { PostData } from '../api/PostData';
+import { useHistory } from 'react-router-dom';
 
 const LoginPage = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    let history = useHistory()
+
+    const handleOnSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        const data = {
+            "username": username,
+            "password": password
+        }
+
+        const response = await PostData("http://localhost:8080/api/user/login", data)
+        console.log(response);
+        if (response.ok) {
+            history.push("/");
+        } else {
+            alert("Login failed")
+        }
+    }
+
     return (
-        <div>
+        <form onSubmit={handleOnSubmit}>
             <h1>Login</h1>
             <label htmlFor="username">Username</label>
-            <input type="text" placeholder="Enter Username" id="username" />
+            <input type="text" placeholder="Enter Username" id="username" value={username} onChange={e => setUsername(e.target.value)} required/>
             <label htmlFor="password">Password</label>
-            <input type="password" placeholder="Enter Password" id="password"/>
-            <button>Login</button>
+            <input type="password" placeholder="Enter Password" id="password" value={password} onChange={e => setPassword(e.target.value)} required/>
+            <input type="submit" value="Login"/>
             <div>
                 <p>Not registered? <a href="/register">Register</a></p>
                 <p>Forgot password? Too bad!!</p>
             </div>
-        </div>
+        </form>
     )
 }
 
