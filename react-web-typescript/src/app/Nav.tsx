2913f1795d514f8fb63d965b7021d330634407b5
Marius Fagerland <marius.fagerland@sparebank1.no>
2020-02-20 15:45:47 +0100

    Add overview to nav

--- react-web-typescript/src/app/Nav.tsx
+++ react-web-typescript/src/app/Nav.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { useHistory } from 'react-router';
 
 const NavBar = styled.div`
   text-align: center;
 `;
 
 const Nav = () => {
-  return (
-    <NavBar>
-      <span>Brotime</span>
-      <span>Settings</span>
-    </NavBar>
-  )
+    const history = useHistory();
+
+    const handleOverviewClick = () => {
+        history.push('/overview')
+    }
+
+    return (
+        <NavBar>
+            <span>Brotime</span>
+            <button onClick={handleOverviewClick}>Overview</button>
+        </NavBar>
+    )
 }
 
 export default Nav;
