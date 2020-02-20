ee768656f5aff5b5a7a305a7efddefe2c5474cfd
Marius Fagerland <marius.fagerland@sparebank1.no>
2020-02-20 12:37:05 +0100

    Fix spacing

--- react-web-typescript/src/app/Routes.tsx
+++ react-web-typescript/src/app/Routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import OverviewPage from './OverviewPage';
 import RegisterPage from './RegisterPage';
