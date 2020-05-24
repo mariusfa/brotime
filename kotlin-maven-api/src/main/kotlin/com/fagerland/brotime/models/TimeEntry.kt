179840050a2895b99a033b5e44a4228fb20e14b7
Marius Fagerland <marius.fagerland@sparebank1.no>
2020-05-24 15:25:09 +0200

    Optimize imports

--- kotlin-maven-api/src/main/kotlin/com/fagerland/brotime/models/TimeEntry.kt
+++ kotlin-maven-api/src/main/kotlin/com/fagerland/brotime/models/TimeEntry.kt
@@ -1,7 +1,12 @@
 package com.fagerland.brotime.models
 
 import com.fasterxml.jackson.annotation.JsonIgnore
-import javax.persistence.*
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.ManyToOne
+import javax.persistence.Table
 
 @Entity
 @Table(name = "timeEntry")
