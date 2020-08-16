db26da685e0c6c4a16b2cd86e0ee3c58c172a6a8
Marius Fagerland <mariusfagerland@yahoo.no>
2020-08-16 12:36:35 +0200

    Fix build

--- kotlin-maven-api/src/main/kotlin/com/fagerland/brotime/services/TimeService.kt
+++ kotlin-maven-api/src/main/kotlin/com/fagerland/brotime/services/TimeService.kt
@@ -8,7 +8,9 @@ import com.fagerland.brotime.repositories.TimeRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
 
+@Service
 class TimeService @Autowired constructor(
         val timeRepository: TimeRepository
 ) {
