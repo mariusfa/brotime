fe2b860123eda7fce42cb2cbf79c42193b08e9d6
mariusfa <mariusfagerland@yahoo.no>
2021-03-18 21:05:12 +0100

    Create HealthControllerTests.kt

---
+++ kotlin-maven-api/src/test/kotlin/com/fagerland/brotime/controllers/HealthControllerTests.kt
@@ -0,0 +1,25 @@
+package com.fagerland.brotime.controllers
+
+import com.fagerland.brotime.repositories.UserRepository
+import com.ninjasquad.springmockk.MockkBean
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@WebMvcTest(controllers = [HealthController::class])
+class HealthControllerTests(@Autowired val mockMvc: MockMvc) {
+
+    @MockkBean
+    private lateinit var userRepository: UserRepository
+
+    @Test
+    fun `Check health path`() {
+        mockMvc.perform(get("/api/health"))
+            .andExpect(status().isOk)
+            .andExpect(content().string("Healthy"))
+    }
+}
