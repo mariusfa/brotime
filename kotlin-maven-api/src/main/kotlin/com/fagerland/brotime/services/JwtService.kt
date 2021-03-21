912bd3d07c16b83c444a2dada2d7cb934feee5bb
mariusfa <mariusfagerland@yahoo.no>
2021-03-21 14:25:03 +0100

    Refactor getUsernameFromRequest

--- kotlin-maven-api/src/main/kotlin/com/fagerland/brotime/services/JwtService.kt
+++ kotlin-maven-api/src/main/kotlin/com/fagerland/brotime/services/JwtService.kt
@@ -32,17 +32,10 @@ class JwtService(
 
     fun getUsernameFromRequest(request: HttpServletRequest): String? {
         val authHeader = request.getHeader("Authentication")
-        if (authHeader.length >= HEADER_TOKEN_INDEX) {
-            val token = authHeader.substring(HEADER_TOKEN_INDEX)
-            val username = getUsernameFromToken(token)
-            if (username != null) {
-                val userEntity = userRepository.findFirstByUsername(username)
-                if (userEntity != null) {
-                    return userEntity.username
-                }
-            }
-        }
-        return null
+        if (authHeader.length < HEADER_TOKEN_INDEX) return null
+        val token = authHeader.substring(HEADER_TOKEN_INDEX)
+        val username = getUsernameFromToken(token) ?: return null
+        return userRepository.findFirstByUsername(username)?.username
     }
 
     fun getUsernameFromToken(token: String?): String? {
