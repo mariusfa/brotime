1479f41931287f37b5b114a9142122152267bac4
mariusfa <mariusfagerland@yahoo.no>
2021-04-08 23:05:15 +0200

    Implement User login int test

--- kotlin-maven-api/src/main/kotlin/com/fagerland/brotime/BrotimeApplication.kt
+++ kotlin-maven-api/src/main/kotlin/com/fagerland/brotime/BrotimeApplication.kt
@@ -1,11 +1,30 @@
 package com.fagerland.brotime
 
+import com.fagerland.brotime.dto.requests.UserDTO
+import com.fagerland.brotime.services.UserService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
 class BrotimeApplication
 
 fun main(args: Array<String>) {
 	runApplication<BrotimeApplication>(*args)
+}
+
+
+@Component
+@Profile("test")
+class TestDataRunner (
+	@Autowired val userService: UserService
+) : CommandLineRunner {
+
+	override fun run(vararg args: String?) {
+		val testUser = UserDTO("testUser", "testPassword")
+		userService.registerUser(testUser)
+	}
 }
--- kotlin-maven-api/src/test/kotlin/com/fagerland/brotime/integrations/UserIntTests.kt
+++ kotlin-maven-api/src/test/kotlin/com/fagerland/brotime/integrations/UserIntTests.kt
@@ -2,16 +2,19 @@ package com.fagerland.brotime.integrations
 
 import com.fagerland.brotime.dto.requests.UserDTO
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 class UserIntTests(
     @Autowired val mockMvc: MockMvc,
@@ -26,5 +29,19 @@ class UserIntTests(
             .contentType(MediaType.APPLICATION_JSON)
             .content(objectMapper.writeValueAsString(user)))
             .andExpect(status().isCreated)
+    }
+
+    @Test
+    fun `Login user`() {
+        val user = UserDTO("testUser", "testPassword")
+
+        val result = mockMvc.perform(post("/api/user/login")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(user)))
+            .andExpect(status().isOk)
+            .andReturn()
+
+        val token = result.response.contentAsString
+        assertThat(token).isNotEmpty
     }
 }
