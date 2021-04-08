089593498e42fdf1ab79c2e71686766f832c5d47
mariusfa <mariusfagerland@yahoo.no>
2021-04-08 23:29:49 +0200

    Fix unused import

--- kotlin-maven-api/src/test/kotlin/com/fagerland/brotime/controllers/TimeControllerTests.kt
+++ kotlin-maven-api/src/test/kotlin/com/fagerland/brotime/controllers/TimeControllerTests.kt
@@ -20,7 +20,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @WebMvcTest(controllers = [TimeController::class])
