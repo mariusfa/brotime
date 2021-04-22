de1c36c44fd9f308112fcd7ef07b82547b3b016d
mariusfa <mariusfagerland@yahoo.no>
2021-04-22 13:10:18 +0200

    Fix indent

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { Login, Register } from './features/UserAuth'
+import { Login, Register } from './features/UserAuth';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dashboard from './features/Dashboard/Dashboard';
 
-function App() {
-	return (
-		<>
-			<Register />
-		</>
-	);
+const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route path='/login' exact>
+                    <Login />
+                </Route>
+                <Route path='/register' exact>
+                    <Register />
+                </Route>
+                <Route path='/dashboard' exact>
+                    <Dashboard />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
-export default App;
+export default App
