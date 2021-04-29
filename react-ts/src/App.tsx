f65aa974b1c7e99df7e049a4f56a473a2ea419dd
mariusfa <mariusfagerland@yahoo.no>
2021-04-29 23:14:27 +0200

    Update default route

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Login, Register } from './features/UserAuth';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Dashboard from './features/Dashboard/Dashboard';
 
 const App = () => {
@@ -9,16 +9,19 @@ const App = () => {
             <Switch>
                 <Route path='/login' exact>
                     <Login />
-            </Route >
+                </Route>
                 <Route path='/register' exact>
                     <Register />
                 </Route>
                 <Route path='/dashboard' exact>
                     <Dashboard />
                 </Route>
+                <Route path='/' exact>
+                    <Redirect to='/dashboard' />
+                </Route>
             </Switch>
         </Router>
     );
-}
+};
 
-export default App
+export default App;
